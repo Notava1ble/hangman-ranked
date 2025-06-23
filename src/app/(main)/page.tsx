0b7e5eab4b0a87ca25e76b7a924af410ed5c8ffc
Visar <visarmullaj07@gmail.com>
2025-06-24 00:04:14 +0200

    Refactor page component to use fetchQuery for user data and add utility classes for centering elements

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,64 +1,72 @@
-"use client";
-
-import { useMutation, useQuery } from "convex/react";
+import { fetchQuery } from "convex/nextjs";
 import { api } from "../../../convex/_generated/api";
-import { useState } from "react";
-import { useAuthActions } from "@convex-dev/auth/react";
+import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
 
-const Page = () => {
-  const [previousGameStats, setPreviousGameStats] = useState<{
-    isCompleted: boolean;
-    isWon: boolean;
-    score: number;
-    attempts: number;
-    mistakes: number;
-    timeTaken?: number;
-  } | null>(null);
+const Page = async () => {
+  // const [previousGameStats, setPreviousGameStats] = useState<{
+  //   isCompleted: boolean;
+  //   isWon: boolean;
+  //   score: number;
+  //   attempts: number;
+  //   mistakes: number;
+  //   timeTaken?: number;
+  // } | null>(null);
 
-  const { signOut } = useAuthActions();
+  // const { signOut } = useAuthActions();
 
-  const user = useQuery(api.auth.loggedInUser);
-  const displayWord = useQuery(api.game.getDisplayWord);
-  const guesses = useQuery(api.game.getGuesses);
+  // const user = useQuery(api.auth.loggedInUser);
+  // const displayWord = useQuery(api.game.getDisplayWord);
+  // const guesses = useQuery(api.game.getGuesses);
 
-  const makeGuess = useMutation(api.game.makeGuess);
-  const startNewGame = useMutation(api.game.createGame);
+  // const makeGuess = useMutation(api.game.makeGuess);
+  // const startNewGame = useMutation(api.game.createGame);
 
-  const handleGuess = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const guessInput = event.currentTarget.elements.namedItem(
-      "guess"
-    ) as HTMLInputElement;
-    const guess = guessInput.value.trim().toLowerCase();
-    if (guess) {
-      try {
-        const stats = await makeGuess({ guess });
-        guessInput.value = "";
-        if (stats) {
-          setPreviousGameStats({
-            isCompleted: stats.isCompleted,
-            isWon: stats.isWon,
-            score: stats.score,
-            attempts: stats.attempts,
-            mistakes: stats.mistakes,
-            timeTaken: stats.timeTaken,
-          });
-        }
-      } catch (error) {
-        console.error("Error making guess:", error);
-      }
-    }
-  };
-  const handleNewGame = async () => {
-    try {
-      await startNewGame();
-    } catch (error) {
-      console.error("Error starting new game:", error);
-    }
-  };
+  // const handleGuess = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   const guessInput = event.currentTarget.elements.namedItem(
+  //     "guess"
+  //   ) as HTMLInputElement;
+  //   const guess = guessInput.value.trim().toLowerCase();
+  //   if (guess) {
+  //     try {
+  //       const stats = await makeGuess({ guess });
+  //       guessInput.value = "";
+  //       if (stats) {
+  //         setPreviousGameStats({
+  //           isCompleted: stats.isCompleted,
+  //           isWon: stats.isWon,
+  //           score: stats.score,
+  //           attempts: stats.attempts,
+  //           mistakes: stats.mistakes,
+  //           timeTaken: stats.timeTaken,
+  //         });
+  //       }
+  //     } catch (error) {
+  //       console.error("Error making guess:", error);
+  //     }
+  //   }
+  // };
+  // const handleNewGame = async () => {
+  //   try {
+  //     await startNewGame();
+  //   } catch (error) {
+  //     console.error("Error starting new game:", error);
+  //   }
+  // };
+
+  const user = await fetchQuery(
+    api.auth.loggedInUser,
+    {},
+    { token: await convexAuthNextjsToken() }
+  );
+
   return (
-    <div>
-      <p className="text-2xl">Hello THere</p>
+    <div className="w-full">
+      <div className="w-full flex-center pt-16">
+        <h1 className="text-4xl font-semibold">
+          Hello there {user ? user.name : "Player"}
+        </h1>
+      </div>
     </div>
   );
 };
