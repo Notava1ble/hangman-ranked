49c4fec97a3cfb19d1024b71c1132667b08aba25
Visar <visarmullaj07@gmail.com>
2025-06-23 14:31:42 +0200

    removed auth with email/pass UI

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,8 +5,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 import SignInBtn from "./SignInBtn";
 
@@ -24,19 +21,19 @@ export default function LoginPage() {
         <div className="flex flex-col gap-6">
           <Card>
             <CardHeader className="text-center">
-              <CardTitle className="text-xl">Welcome back</CardTitle>
+              <CardTitle className="text-xl">Welcome</CardTitle>
               <CardDescription>
                 Login with your Google or Github account
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <form>
-                <div className="grid gap-6">
-                  <div className="flex flex-col gap-4">
-                    <SignInBtn provider="Google" />
-                    <SignInBtn provider="GitHub" />
-                  </div>
-                  <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+              {/* <form> */}
+              <div className="grid gap-6">
+                <div className="flex flex-col gap-4">
+                  <SignInBtn provider="Google" />
+                  <SignInBtn provider="GitHub" />
+                </div>
+                {/* <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                     <span className="bg-card text-muted-foreground relative z-10 px-2">
                       Or continue with
                     </span>
@@ -64,9 +61,9 @@ export default function LoginPage() {
                     <a href="#" className="underline underline-offset-4">
                       Sign up
                     </a>
-                  </div>
-                </div>
-              </form>
+                  </div> */}
+              </div>
+              {/* </form> */}
             </CardContent>
           </Card>
         </div>
