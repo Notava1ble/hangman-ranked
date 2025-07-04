15d600d171a7cd2599042c0365a5350cd2749431
Visar <visarmullaj07@gmail.com>
2025-07-04 23:48:36 +0200

    apparently its not working with .env so i need to hardcode it. I dont think this would be a problem

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.SENTRY_DSN,
+  dsn: "https://4c430b83554a50a89ddb7c96caba3b62@o4509612126306304.ingest.de.sentry.io/4509612127551568",
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
