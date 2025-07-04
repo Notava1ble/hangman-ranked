95793b888490a2f1205d43a9bde53ff92ec9cfaa
Visar <visarmullaj07@gmail.com>
2025-07-04 22:23:20 +0200

    small update

--- src/app/(main)/leaderboard/loading.tsx
+++ src/app/(main)/leaderboard/loading.tsx
@@ -11,7 +11,6 @@ export const revalidate = 1800; // 30 min
 const Loading = () => {
   return (
     <div className="flex-center w-full h-full">
-      {/* TODO: Make the UI responsive and then deploy */}
       <div className="bg-white rounded-md drop-shadow-lg w-[95%] lg:w-[55%] md:w-[75%] sm:w-[85%] mt-6 mb-6">
         <div className="p-6 bg-indigo-600 rounded-t-md text-white">
           <h2 className="text-3xl font-bold">🏆 Leaderboard</h2>
