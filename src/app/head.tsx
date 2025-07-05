e9180ec681770e29eb961f646d6dce4379345a84
Visar <visarmullaj07@gmail.com>
2025-07-05 19:29:29 +0200

    Changed title and added plausable

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <script
+        defer
+        data-domain="hangman-ranked.vercel.app"
+        src="https://plausible.io/js/script.tagged-events.js"
+      ></script>
+    </>
+  );
+}
