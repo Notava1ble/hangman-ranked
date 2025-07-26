35ebe6acbaa7e3d94a94a825849c2c7a90d2c58b
Visar <visarmullaj07@gmail.com>
2025-07-26 22:51:29 +0200

    Overwrote the createUser function do add a default elo

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,10 +1,21 @@
 import GitHub from "@auth/core/providers/github";
 import Google from "@auth/core/providers/google";
 import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
-import { query, QueryCtx } from "./_generated/server";
+import { MutationCtx, query, QueryCtx } from "./_generated/server";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
   providers: [GitHub, Google],
+  callbacks: {
+    async createOrUpdateUser(ctx: MutationCtx, args) {
+      if (args.existingUserId) {
+        return args.existingUserId;
+      }
+      return ctx.db.insert("users", {
+        ...args,
+        elo: 1200,
+      });
+    },
+  },
 });
 
 export const loggedInUser = query({
